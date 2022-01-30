915d942056b4b326bfe074ed66f5d04ac30384f6
Alexej Klestov <klestov@hotmail.com>
2022-01-30 14:58:30 +0300

    Examples

--- Common/ExceptionPolicy.cs
+++ Common/ExceptionPolicy.cs
@@ -1,11 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
 using ShtrihM.Wattle3.Common.Exceptions;
 using ShtrihM.Wattle3.DomainObjects.EntryPoints;
 using ShtrihM.Wattle3.DomainObjects.Interfaces;
