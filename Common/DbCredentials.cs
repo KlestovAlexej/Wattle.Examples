59f6f1265e38cf7665775e501eb885bc6d8b5907
Alexej Klestov <klestov@hotmail.com>
2022-02-02 18:44:17 +0300

    e

---
+++ Common/DbCredentials.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using ShtrihM.Wattle3.Testing;
+
+namespace ShtrihM.Wattle3.Examples.Common
+{
+    /// <summary>
+    /// Параметры учётных записей подключения к БД.
+    /// </summary>
+    public static class DbCredentials
+    {
+        /// <summary>
+        /// Пример реестра Windows.
+        /// </summary>
+        public static readonly string RegistryFile = @"Windows Registry Editor Version 5.00
+
+[HKEY_LOCAL_MACHINE\SOFTWARE\Shtrih-M\Testing]
+""PostgreSqlUserID""=""postgres""
+""PostgreSqlPassword""=""PostgreSqlPassword""
+""SqlServerUserID""=""sa""
+""SqlServerPassword""=""SqlServerPassword""
+""SqlServerAdress""=""localhost""
+""PostgreSqlAdress""=""localhost""
+
+[HKEY_LOCAL_MACHINE\SOFTWARE\WOW6432Node\Shtrih-M\Testing]
+""PostgreSqlUserID""=""postgres""
+""PostgreSqlPassword""=""PostgreSqlPassword""
+""SqlServerUserID""=""sa""
+""SqlServerPassword""=""SqlServerPassword""
+""SqlServerAdress""=""localhost""
+""PostgreSqlAdress""=""localhost""";
+
+        /// <summary>
+        /// Адрес сервера с SQL Server.
+        /// По умолчанию адрес читается из реестра Windows;
+        /// Пример реестра Windows <see cref="RegistryFile"/>.
+        /// </summary>
+        public static bool TryGetServerAdressForSqlServer(out string serverAdress)
+        {
+            /*
+            serverAdress = "localhost";
+
+            return true;
+            */
+
+            var result = WindowsRegistryHelpers.SqlServerTryGetServerAdress(out serverAdress);
+
+            return result;
+        }
+
+        /// <summary>
+        /// Адрес сервера с PostgreSQL.
+        /// По умолчанию адрес читается из реестра Windows;
+        /// Пример реестра Windows <see cref="RegistryFile"/>.
+        /// </summary>
+        public static bool TryGetServerAdressForPostgreSql(out string serverAdress)
+        {
+            /*
+            serverAdress = "localhost";
+
+            return true;
+            */
+
+            var result = WindowsRegistryHelpers.PostgreSqlTryGetServerAdress(out serverAdress);
+
+            return result;
+        }
+
+        /// <summary>
+        /// Параметры учётных записей подключения к SQL Server.
+        /// По умолчанию параметры читаются из реестра Windows;
+        /// Пример реестра Windows <see cref="RegistryFile"/>.
+        /// </summary>
+        public static bool TryGetCredentialsForSqlServer(out (string UserName, string UserPassword) credentials)
+        {
+            /*
+            credentials = ("sa", "password");
+
+            return true;
+            */
+
+            var result = WindowsRegistryHelpers.SqlServerTryGetUserCredentials(out credentials);
+
+            return result;
+        }
+
+        /// <summary>
+        /// Параметры учётных записей подключения к PostgreSQL.
+        /// По умолчанию параметры читаются из реестра Windows;
+        /// Пример реестра Windows <see cref="RegistryFile"/>.
+        /// </summary>
+        public static bool TryGetCredentialsForPostgreSql(out (string UserName, string UserPassword) credentials)
+        {
+            /*
+            credentials = ("postgres", "password");
+
+            return true;
+            */
+
+            var result = WindowsRegistryHelpers.PostgreSqlTryGetUserCredentials(out credentials);
+
+            return result;
+        }
+    }
+}
---
+++ Mappers/PostgreSql/Common/SqlScripts/SqlScript.sql
@@ -0,0 +1,35 @@
+﻿SET check_function_bodies = false;
+
+/* Sequence 'Sequence_Object_A' */
+CREATE SEQUENCE Sequence_Object_A START WITH 1 INCREMENT BY 1;
+
+/* Table 'object_b' */
+CREATE TABLE object_b(
+  id bigint NOT NULL,
+  revision bigint NOT NULL,
+  createdate timestamp NOT NULL,
+  modificationdate timestamp NOT NULL,
+  state smallint NOT NULL,
+  PRIMARY KEY(id)
+);
+
+/* Table 'object_a' */
+CREATE TABLE object_a(
+  id bigint NOT NULL,
+  revision bigint NOT NULL,
+  createdate timestamp NOT NULL,
+  modificationdate timestamp NOT NULL,
+  state smallint NOT NULL,
+  PRIMARY KEY(id)
+) PARTITION BY RANGE (id);
+
+/* Table 'object_c' */
+CREATE TABLE object_c(
+  id bigint NOT NULL,
+  revision bigint NOT NULL,
+  createdate timestamp NOT NULL,
+  modificationdate timestamp NOT NULL,
+  state smallint NOT NULL,
+  available bit NOT NULL,
+  PRIMARY KEY(id)
+);
